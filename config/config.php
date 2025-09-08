ab8e5cef6a2da60c90cfe188f85f3a18f0ae7f46
Joey Smith <jsmith@webinertia.net>
2025-09-07 20:48:09 -0500

    initial commit post install of all deps. Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

---
+++ config/config.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use Laminas\ConfigAggregator\ArrayProvider;
+use Laminas\ConfigAggregator\ConfigAggregator;
+use Laminas\ConfigAggregator\PhpFileProvider;
+
+// To enable or disable caching, set the `ConfigAggregator::ENABLE_CACHE` boolean in
+// `config/autoload/local.php`.
+$cacheConfig = [
+    'config_cache_path' => 'data/cache/config-cache.php',
+];
+
+$aggregator = new ConfigAggregator([
+    \PhpDb\Adapter\Mysql\ConfigProvider::class,
+    \PhpDb\Container\ConfigProvider::class,
+    \Laminas\ServiceManager\ConfigProvider::class,
+    \Mezzio\Tooling\ConfigProvider::class,
+    \Mezzio\Plates\ConfigProvider::class,
+    \Mezzio\Router\FastRouteRouter\ConfigProvider::class,
+    \Laminas\HttpHandlerRunner\ConfigProvider::class,
+    // Include cache configuration
+    new ArrayProvider($cacheConfig),
+    \Mezzio\Helper\ConfigProvider::class,
+    \Mezzio\ConfigProvider::class,
+    \Mezzio\Router\ConfigProvider::class,
+    \Laminas\Diactoros\ConfigProvider::class,
+    // Default App module config
+    App\ConfigProvider::class,
+    // Load application config in a pre-defined order in such a way that local settings
+    // overwrite global settings. (Loaded as first to last):
+    //   - `global.php`
+    //   - `*.global.php`
+    //   - `local.php`
+    //   - `*.local.php`
+    new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
+    // Load development config if it exists
+    new PhpFileProvider(realpath(__DIR__) . '/development.config.php'),
+], $cacheConfig['config_cache_path']);
+
+return $aggregator->getMergedConfig();
